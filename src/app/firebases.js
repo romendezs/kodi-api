d38c54710f0566a1b38f6a2bbe4cce23389bf2a9
Gómez David <david.gomez.vasquez1998@gmail.com>
2024-11-01 20:53:35 -0600

    Modificacion en el archivo env para seguridad

--- src/app/firebases.js
+++ src/app/firebases.js
@@ -1,22 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVUX7F19Au_saRRMZRJK4812CS57Ok6Rc",
-  authDomain: "bootcamp-login.firebaseapp.com",
-  projectId: "bootcamp-login",
-  storageBucket: "bootcamp-login.appspot.com",
-  messagingSenderId: "1019434583543",
-  appId: "1:1019434583543:web:f157e59087e7ac6c05daf2",
-  measurementId: "G-DJHDVJX4H5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
